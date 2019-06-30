acfe97e727df0061c72a2985b56036062092a4c9
Christian Passold <passold.christian@hotmail.com>
2019-06-30 11:05:40 -0300

    3d

---
+++ TrabalhoFinal3D/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TrabalhoFinal3D
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Render window = new Render(600, 600);
+            window.Run();
+            window.Run(1.0 / 60.0);
+        }
+    }
+}
